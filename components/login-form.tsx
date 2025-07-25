cae01556746e0ba3597c38d60f7d20b891faf75e
Metapia <metapiascl@gmail.com>
2025-07-25 16:20:13 -0400

    login with ZOD ok

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,64 +1,126 @@
-import Image from "next/image"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+"use client";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+//import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+
+const loginSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+  })
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+    const form = useForm<z.infer<typeof loginSchema>>({
+      resolver: zodResolver(loginSchema),
+      defaultValues: {
+        email: "",
+        password: "",
+      },
+    });
+
+    // 2. Define a submit handler.
+      function onSubmit(values: z.infer<typeof loginSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+if (values.email === "metapiascl@gmail.com" && values.password === "Batman") {
+          alert("Login successful!");
+        } else {
+          alert("Invalid email or password. Please try again.");
+        }
+
+      }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-balance text-muted-foreground">
-                  Login to your NDCM Inc account
-                </p>
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className="text-balance text-muted-foreground">
+                    Login to your NDCM Inc account
+                  </p>
                 </div>
-                <Input id="password" type="password" required />
+                <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs font-medium text-gray-900">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Enter Email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs font-medium text-gray-900">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+                <Button type="submit" className="w-full  bg-blue-500 text-white hover:bg-blue-700">
+                  Login
+                </Button>
               </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-             
-              
-            </div>
-          </form>
+            </form>
+          </Form>
+
           <div className="relative hidden bg-muted md:block">
-          <Image 
-                    src="/dc1.jpg"
-                    width={500}
-                    height={500}
-                    alt="Image"
-                   className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                  />
+            <Image
+              src="/dc1.jpg"
+              width={500}
+              height={500}
+              alt="Image"
+              priority
+              className="absolute inset-0 h-full w-full object-cover"
+            />
           </div>
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our Terms of Service
+        and Privacy Policy.
       </div>
     </div>
-  )
+  );
 }
